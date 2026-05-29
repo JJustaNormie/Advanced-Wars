05b7f19bee97cdbce768d9bdb39922df7abba46c
JJustaNormie <matheus.aug.11@gmail.com>
2026-05-29 11:34:14 -0300

    Move and Turns

--- AdvWar.m
+++ AdvWar.m
@@ -10,6 +10,9 @@ function Confirm(jogo, rep)
     jogo.set_input([0 0 0 0 0 0 0 0]);
     jogo.step(10);
     jogo.set_input([0 0 0 0 0 0 1 0]);
+    tela=jogo.get_image();
+    imshow(tela);
+    drawnow;
     jogo.step(300);
   end
 end
@@ -52,40 +55,7 @@ function final=detect(img, pad, final, y, x)
   final=final+cand((1+y):(160+y),(1+x):(240+x),:);
 end
 
-function [matriz, quant]=visao(img, matrix)
-
-  infantry=img(94:109,34:47,:);
-  mech=img(95:109,49:64,:);
-  recon=img(127:141,19:31,:);
-  Ltank=img(82:93,19:30,:);
-  apc=img(129:141,34:47,:);
-  artillery=img(96:109,2:16,:);
-
-  final=zeros(160,240);
-
-  final=detect(img, infantry, final, 15, 13);
-  final=detect(img, mech, final, 14, 15);
-  final=detect(img, recon, final, 14, 12);
-  final=detect(img, Ltank, final, 11, 11);
-  final=detect(img, apc, final, 12, 13);
-  final=detect(img, artillery, final, 13, 14);
-
-  quant=0;
-  for i=1:10
-    for j=1:15
-      if final((16*i)-8,(16*j)-8,:)>0
-        matriz(i,j,:)=1;
-        quant=quant+1;
-      else
-        matriz(i,j,:)=0;
-      end
-    endfor
-  endfor
-end
-
-% [start select up down left right a b]
-
-function UnitMove(Dist, jogo)
+function moving(Dist,jogo)
   if Dist(1)<0
     for i=1:sqrt(Dist(1)*Dist(1))
       jogo.set_input([0 0 1 0 0 0 0 0]);
@@ -118,20 +88,193 @@ function UnitMove(Dist, jogo)
       jogo.step(10);
     endfor
   endif
+end
+
+function [tela1,tela2,cursor]=getimages(cursor,jogo)
+  pt1=[6,4];
+  pt2=[6,12];
+
+  Dist=pt1-cursor;
+  cursor=pt1;
+  moving(Dist,jogo);
+  tela1=jogo.get_image();
+
+  Dist=pt2-cursor;
+  cursor=pt2;
+  moving(Dist,jogo);
+  tela2=jogo.get_image();
+end
+
+function [matriz, quant]=visao(imginit, img1, img2, matrix)
+
+  infantry=imginit(94:109,34:47,:);
+  mech=imginit(95:109,49:64,:);
+  recon=imginit(127:141,19:31,:);
+  Ltank=imginit(82:93,19:30,:);
+  apc=imginit(129:141,34:47,:);
+  artillery=imginit(96:109,2:16,:);
+
+  final=zeros(160,240);
+
+  final=detect(img1, infantry, final, 15, 13);
+  final=detect(img1, mech, final, 14, 15);
+  final=detect(img1, recon, final, 14, 12);
+  final=detect(img1, Ltank, final, 11, 11);
+  final=detect(img1, apc, final, 12, 13);
+  final=detect(img1, artillery, final, 13, 14);
+
+  final=detect(img2, infantry, final, 15, 13);
+  final=detect(img2, mech, final, 14, 15);
+  final=detect(img2, recon, final, 14, 12);
+  final=detect(img2, Ltank, final, 11, 11);
+  final=detect(img2, apc, final, 12, 13);
+  final=detect(img2, artillery, final, 13, 14);
+
+  imshow(final);
+  drawnow;
+  pause;
+  matriz=zeros(10, 15);
+
+  quant=0;
+  for i=1:10
+    for j=1:15
+      if final((16*i)-8,(16*j)-8,2)>0|final((16*i)-8,(16*j)-8,1)>0|final((16*i)-8,(16*j)-8,3)>0
+        matriz(i,j,:)=1;
+        quant=quant+1;
+      end
+    endfor
+  endfor
+end
+
+% [start select up down left right a b]
+
+function [posf,matrix]=UnitMove(Dist, pos, matrix, jogo)
+  posinit=pos;
+  moving(Dist,jogo);
+  tela=rgb2gray(jogo.get_image());
 
   jogo.set_input([0 0 0 0 0 0 1 0]);
   jogo.step(10);
   jogo.set_input([0 0 0 0 0 0 0 0]);
   jogo.step(10);
 
-  tela=jogo.get_image();
-  img=rgb2hsv(tela);
-  V=img(:,:,3);
-  Range=(V>0.95);
+  telacomp=rgb2gray(jogo.get_image());
+  moveop = telacomp - tela;
 
-  imshow(Range);
+  for i=1:10
+    for j=1:15
+      if matrix(i,j)>0
+        moveop((i*16)-8,(j*16)-8,:)=0;
+      end
+    end
+  end
+
+  if pos(2)<8
+    matmov=zeros(10,8);
+    for i=1:10
+      for j=1:7
+        if moveop((i*16)-8,(j*16)-8,:)>0
+          matmov(i,j)=3;
+        end
+      end
+    end
+    desloc=pos;
+    menor=[99,99];
+    alvo=[2,15];
+    for i=1:10
+      for j=1:7
+        if matmov(i,j)==3;
+          itar=sqrt((i-alvo(1))*(i-alvo(1)));
+          jtar=sqrt((alvo(2)-j)*(alvo(2)-j));
+          if itar+jtar<menor(1)+menor(2)
+            menor(1)=itar;
+            menor(2)=jtar;
+            desloc=[i,j];
+          endif
+        end
+      end
+    end
+  endif
+  if pos(2)>|
+    matmov=zeros(10,7);
+    for i=1:10
+      for j=1:8
+        if moveop((i*16)-8,((j+7)*16)-8,:)>0
+          matmov(i,j)=3;
+        end
+      end
+    end
+    desloc=pos;
+    menor=[99,99];
+    alvo=[2,15];
+    for i=1:10
+      for j=1:8
+        if matmov(i,j)==3;
+          itar=sqrt((i-alvo(1))*(i-alvo(1)));
+          jtar=sqrt((alvo(2)-j)*(alvo(2)-j));
+          if itar+jtar<menor(1)+menor(2)
+            menor(1)=itar;
+            menor(2)=jtar;
+            desloc=[i,j];
+          endif
+        end
+      end
+    end
+  endif
+  if pos(2)>7
+    matmov=zeros(10,7);
+    for i=1:10
+      for j=1:8
+        if moveop((i*16)-8,((j+7)*16)-8,:)>0
+          matmov(i,j)=3;
+        end
+      end
+    end
+    desloc=pos;
+    menor=[99,99];
+    alvo=[2,15];
+    for i=1:10
+      for j=1:8
+        if matmov(i,j)==3;
+          itar=sqrt((i-alvo(1))*(i-alvo(1)));
+          jtar=sqrt((alvo(2)-j)*(alvo(2)-j));
+          if itar+jtar<menor(1)+menor(2)
+            menor(1)=itar;
+            menor(2)=jtar;
+            desloc=[i,j];
+          endif
+        end
+      end
+    end
+  endif
+
+  Dist=desloc-pos;
+  pos=desloc;
+
+  moving(Dist,jogo);
+
+  if posinit(1)==pos(1)&posinit(2)==pos(2)
+    jogo.set_input([0 0 0 0 0 0 0 1]);
+    jogo.step(10);
+    jogo.set_input([0 0 0 0 0 0 0 0]);
+    jogo.step(60);
+  else
+    jogo.set_input([0 0 0 0 0 0 1 0]);
+    jogo.step(10);
+    jogo.set_input([0 0 0 0 0 0 0 0]);
+    jogo.step(60);
+    jogo.set_input([0 0 0 0 0 0 1 0]);
+    jogo.step(10);
+    jogo.set_input([0 0 0 0 0 0 0 0]);
+    jogo.step(300);
+  endif
+
+  tela=jogo.get_image();
+  imshow(tela);
   drawnow;
-  pause
+  pause;
+  matrix(pos(1), pos(2)) = 2;
+  posf=pos;
 end
 
 function cursor=movement(quant, cursor, matrix, jogo)
@@ -140,7 +283,7 @@ function cursor=movement(quant, cursor, matrix, jogo)
     for i=1:10
       for j=1:15
         if matrix(i,j)==1
-          pos=[i,j]
+          pos=[i,j];
           break;
         end
       end
@@ -150,7 +293,9 @@ function cursor=movement(quant, cursor, matrix, jogo)
     end
     Dist=pos-cursor;
 
-    UnitMove(Dist, jogo)
+    [posf, matrix]=UnitMove(Dist, pos, matrix, jogo);
+    matrix(pos(1),pos(2))=0;
+    cursor=posf;
   endfor
 end
 
@@ -159,28 +304,40 @@ Iniciar(jogo)
 telainit=jogo.get_image();
 telainit=rgb2gray(telainit);
 map=zeros(10, 15);
-cursor=[9,1]
+cursor=[9,1];
 
 for i=1:50
-  tela=jogo.get_image();
-
-  [map,quant]=visao(telainit, map);
+  [tela1,tela2,cursor]=getimages(cursor,jogo);
+  [map,quant]=visao(telainit, tela1, tela2, map);
+  map
 
   cursor=movement(quant, cursor, map, jogo);
+  tela=jogo.get_image();
   imshow(tela);
   drawnow;
-  pause
 
-  img=rgb2hsv(tela);
-  H=img(:,:,1);
-  V=img(:,:,3);
-  Allies=(H==0);
-  Fog=(V>0.8);
-  Fog(1:48,144:end)=0;
-  Fog(1:48,208:end)=0;
-  #Allies = imdilate(Allies,ones(3));
-
+  jogo.set_input([0 0 0 0 0 0 0 1]);
+  jogo.step(10);
+  jogo.set_input([0 0 0 0 0 0 0 0]);
+  jogo.step(300);
+  jogo.set_input([0 0 0 0 0 0 1 0]);
+  jogo.step(10);
+  jogo.set_input([0 0 0 0 0 0 0 0]);
+  jogo.step(3000);
+  tela=jogo.get_image();
   imshow(tela);
   drawnow;
-  hold off
+
+  jogo.set_input([0 0 1 0 0 0 0 0]);
+  jogo.step(10);
+  jogo.set_input([0 0 0 0 0 0 0 0]);
+  jogo.step(10);
+  jogo.set_input([0 0 0 0 0 0 1 0]);
+  jogo.step(10);
+  jogo.set_input([0 0 0 0 0 0 0 0]);
+  jogo.step(3000);
+  tela=jogo.get_image();
+  imshow(tela);
+  drawnow;
+  pause;
 end
